c69867f318b401304678c36881819bcfe5ca7274
Chris Boyd <95505985+conceptcollector@users.noreply.github.com>
2023-09-18 11:47:13 -0500

    Copied Prime's raw App.js file

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  // Renders the entire app on the DOM
+  return (
+    <div>
+      <header className="App-header">
+        <h1>Random Giphy API</h1>
+      </header>
+      
+      <p>Results go here</p>
+    </div>
+  );
+}
+
+export default App;
