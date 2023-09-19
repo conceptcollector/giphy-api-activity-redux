f21cfa8eda750361d1226fbdfcfe93427be561ed
Christopher Boyd <boyd.christopher.t@gmail.com>
2023-09-19 12:09:24 -0500

    Added the Giphy random URL and created the get route

--- server/routes/random.router.js
+++ server/routes/random.router.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World'); // Replace this
+    axios({
+        method: 'GET',
+        url: 'api.giphy.com/v1/gifs/random'
+    }).then((response) => {
+        res.send(response.data);
+    })
 })
 
 module.exports = router;
