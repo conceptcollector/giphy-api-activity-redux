71e6398ba443ba5181f62b7e2030a133ac6f227a
Christopher Boyd <boyd.christopher.t@gmail.com>
2023-09-25 17:29:05 -0500

    Built Saga

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
+import {put, takeLatest} from 'redux-saga/effects';
+import axios from 'axios';
 
 // Reducer that holds our results
 const random = (state = {}, action) => {
@@ -13,6 +15,21 @@ const random = (state = {}, action) => {
         return action.payload;
     }
     return state;
+}
+
+// Saga to send fetch
+function* fetchGif() {
+    
+    const randomGif = yield axios.get('/random');
+
+    yield put({ type: 'SET_RANDOM', payload: randomGif.data });
+
+}
+
+function* gifSaga() {
+
+    yield takeLatest('FETCH_GIF', fetchGif);
+
 }
 
 // Create one store that all components can use
